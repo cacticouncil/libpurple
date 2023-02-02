3a413bd5086e5992164b7888f99ccec1e8a6048d
Cindy Ngo <62029045+cindyngo44@users.noreply.github.com>
2023-02-02 15:38:23 -0500

    Added new lizard UI

--- lizard/exampleapp.h
+++ lizard/exampleapp.h
@@ -7,6 +7,7 @@
 #define EXAMPLE_APP_TYPE (example_app_get_type ())
 G_DECLARE_FINAL_TYPE (ExampleApp, example_app, EXAMPLE, APP, GtkApplication)
 
+
 ExampleApp     *example_app_new         (void);
 
 

a79515306b4182b222fa99a7b511dd419e33070e
Cindy Ngo <62029045+cindyngo44@users.noreply.github.com>
2023-03-02 18:56:35 -0500

    dynamic expander
    work in progress

--- lizard/exampleappwin.c
+++ lizard/exampleappwin.c
@@ -3,16 +3,29 @@
 #include "exampleapp.h"
 #include "exampleappwin.h"
 
+int expanderSize = 1;
 struct _ExampleAppWindow
 {
   GtkApplicationWindow parent;
 
   GtkWidget *send_button;
+  GtkWidget *channels;
 };
 
 G_DEFINE_TYPE(ExampleAppWindow, example_app_window, GTK_TYPE_APPLICATION_WINDOW);
-
-static void 
+void
+generateExpander ()
+{
+  g_print ("Expander Generated\n");
+  GtkWidget *expanderWidget;
+  for (int i = 0; i < expanderSize; i++)
+    {
+      expanderWidget = gtk_expander_new_with_mnemonic ("More Options");
+      gtk_widget_set_halign (expanderWidget, GTK_ALIGN_START);
+      gtk_widget_set_valign (expanderWidget, GTK_ALIGN_START);
+    }
+}
+static void
 send_clicked ()
 {
   g_print ("Message Sent\n");
@@ -29,8 +42,12 @@ example_app_window_class_init (ExampleAppWindowClass *class)
 {
   gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (class),
                                                "/org/gtk/exampleapp/window.ui");
+  //Creates a GtkWidget object that points to the object ID in window.ui
   gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (class), ExampleAppWindow, send_button);
+  gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (class), ExampleAppWindow, channels); //Figure out how to get children
   gtk_widget_class_bind_template_callback (GTK_WIDGET_CLASS (class), send_clicked);
+
+   generateExpander ();
 }
 
 ExampleAppWindow *
